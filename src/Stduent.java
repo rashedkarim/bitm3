635ca5d183aad5049a878577f96632431ffaec5d
TahsinAyman <mail4tahsin@gmail.com>
2023-12-09 12:43:06 +0600

    Student modified

--- src/Stduent.java
+++ src/Stduent.java
@@ -1,4 +1,12 @@
 public class Stduent {
   private int id;
   private String name;
+  private String address;
+
+  public Stduent(int id, String name, String address) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+  
 }
